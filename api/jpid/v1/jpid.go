c8262e58efdcc96face51fae136f1ed9fbcdab9c
tan <13320385140@163.com>
2025-05-06 22:47:25 +0800

    JPID API

--- api/jpid/v1/jpid.go
+++ api/jpid/v1/jpid.go
@@ -2,9 +2,12 @@ package v1
 
 import (
 	"github.com/gogf/gf/v2/frame/g"
+	"omniscient/internal/model/entity"
 )
 
 type JpidReq struct {
 	g.Meta `path:"/jpid" tags:"Java" method:"get" summary:"java 项目列表"`
 }
-type JpidRes struct{}
+type JpidRes struct {
+	List []*entity.Jpid `json:"list" dc:"java 项目列表"`
+}
--- internal/controller/jpid/jpid_v1_jpid.go
+++ internal/controller/jpid/jpid_v1_jpid.go
@@ -2,13 +2,12 @@ package jpid
 
 import (
 	"context"
-
-	"github.com/gogf/gf/v2/errors/gcode"
-	"github.com/gogf/gf/v2/errors/gerror"
+	"omniscient/internal/dao"
 
 	"omniscient/api/jpid/v1"
 )
 
 func (c *ControllerV1) Jpid(ctx context.Context, req *v1.JpidReq) (res *v1.JpidRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	dao.Jpid.Ctx(ctx).Where(dao.Jpid).Scan(&res.List)
+	return
 }
