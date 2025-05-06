6b35a43c2b9ab8e283301250277f9ef0d989a148
tan <13320385140@163.com>
2025-05-06 22:55:32 +0800

    fix

--- internal/controller/jpid/jpid_v1_jpid.go
+++ internal/controller/jpid/jpid_v1_jpid.go
@@ -3,11 +3,12 @@ package jpid
 import (
 	"context"
 	"omniscient/internal/dao"
+	"omniscient/internal/model/do"
 
 	"omniscient/api/jpid/v1"
 )
 
 func (c *ControllerV1) Jpid(ctx context.Context, req *v1.JpidReq) (res *v1.JpidRes, err error) {
-	dao.Jpid.Ctx(ctx).Where(dao.Jpid).Scan(&res.List)
+	dao.Jpid.Ctx(ctx).Where(do.Jpid{}).Scan(&res.List)
 	return
 }
