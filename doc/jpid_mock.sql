c8f2285f3fde1ecddb6a620fd2451ecae63679bf
tan <13320385140@163.com>
2025-05-06 22:29:00 +0800

    sql

--- doc/jpid_mock.sql
+++ doc/jpid_mock.sql
@@ -1,6 +1,6 @@
-INSERT INTO `jpid` (`name`, `port`, `pid`, `catalog`, `run`, `description`) VALUES
-                                                                                ('UserManage', 8080, 123, '/usr/local/java/usermanage', 'sh /usr/local/java/usermanage/start.sh', '用户管理系统'),
-                                                                                ('OrderSys', 8081, 124, '/opt/java/ordersys', 'sh /opt/java/ordersys/run.sh', '订单处理系统'),
-                                                                                ('ProductSys', 8082, 125, '/home/java/productsys', 'sh /home/java/productsys/init.sh', '商品信息管理系统'),
-                                                                                ('AuthSys', 8083, 126, '/var/lib/java/authsys', 'sh /var/lib/java/authsys/launch.sh', '认证授权系统'),
-                                                                                ('ReportSys', 8084, 127, '/root/java/reportsys', 'sh /root/java/reportsys/generate_report.sh', '报表生成系统');
+INSERT INTO `jpid` (`name`, `port`, `pid`, `catalog`, `run`, `status`, `description`) VALUES
+                                                                                          ('UserManage', 8080, 123, '/usr/local/java/usermanage', 'sh /usr/local/java/usermanage/start.sh', 1, '用户管理系统'),
+                                                                                          ('OrderSys', 8081, 124, '/opt/java/ordersys', 'sh /opt/java/ordersys/run.sh', 0, '订单处理系统'),
+                                                                                          ('ProductSys', 8082, 125, '/home/java/productsys', 'sh /home/java/productsys/init.sh', 1, '商品信息管理系统'),
+                                                                                          ('AuthSys', 8083, 126, '/var/lib/java/authsys', 'sh /var/lib/java/authsys/launch.sh', 0, '认证授权系统'),
+                                                                                          ('ReportSys', 8084, 127, '/root/java/reportsys', 'sh /root/java/reportsys/generate_report.sh', 1, '报表生成系统');
--- doc/table.sql
+++ doc/table.sql
@@ -5,6 +5,7 @@ CREATE TABLE `jpid` (
                         `pid` int NOT NULL COMMENT 'pid',
                         `catalog` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '运行目录',
                         `run` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '运行脚本（sh命令',
+                        `status` int DEFAULT '0' COMMENT '状态[1:启动，0:停止]',
                         `description` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '项目描述',
                         PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='java项目详情';
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='java项目详情';
