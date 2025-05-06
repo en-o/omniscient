400226021fb142d6a9223d32d0db5bdd6b733704
tan <13320385140@163.com>
2025-05-06 23:02:23 +0800

    fix

--- internal/controller/jpid/jpid_v1_jpid.go
+++ internal/controller/jpid/jpid_v1_jpid.go
@@ -2,13 +2,18 @@ package jpid
 
 import (
 	"context"
-	"omniscient/internal/dao"
-	"omniscient/internal/model/do"
-
 	"omniscient/api/jpid/v1"
+	"omniscient/internal/dao"
 )
 
 func (c *ControllerV1) Jpid(ctx context.Context, req *v1.JpidReq) (res *v1.JpidRes, err error) {
-	dao.Jpid.Ctx(ctx).Where(do.Jpid{}).Scan(&res.List)
-	return
+	// 初始化 res , 不初始化会出现 invalid memory address or nil pointer dereference
+	res = &v1.JpidRes{}
+
+	err = dao.Jpid.Ctx(ctx).Scan(&res.List)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
