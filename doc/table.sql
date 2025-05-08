9091d67ed5cddb8f4025e54a1a899b78a356f887
tan <1445763190@qq.com>
2025-05-08 14:04:48 +0800

    新增字段

--- doc/table.sql
+++ doc/table.sql
@@ -4,7 +4,8 @@ CREATE TABLE `jpid` (
                         `ports` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '运行端口,多个逗号隔开',
                         `pid` int NOT NULL COMMENT 'pid',
                         `catalog` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '运行目录',
-                        `run` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '运行脚本（sh命令',
+                        `run` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '原生启动命令',
+                        `script` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'sh脚本启动命令',
                         `status` int DEFAULT '0' COMMENT '状态[1:启动，0:停止]',
                         `description` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '项目描述',
                         PRIMARY KEY (`id`)
--- internal/model/entity/jpid.go
+++ internal/model/entity/jpid.go
@@ -8,10 +8,11 @@ package entity
 type Jpid struct {
 	Id          int    `json:"id"          orm:"id"          description:""`              //
 	Name        string `json:"name"        orm:"name"        description:"java项目名"`       // java项目名
-	Ports       string `json:"ports"        orm:"ports"        description:"运行端口,多个逗号隔开"` // 运行端口,多个逗号隔开
+	Ports       string `json:"ports"       orm:"ports"        description:"运行端口,多个逗号隔开"`  // 运行端口,多个逗号隔开
 	Pid         int    `json:"pid"         orm:"pid"         description:"pid"`           // pid
 	Catalog     string `json:"catalog"     orm:"catalog"     description:"运行目录"`          // 运行目录
-	Run         string `json:"run"         orm:"run"         description:"运行脚本（sh命令"`     // 运行脚本（sh命令
+	Run         string `json:"run"         orm:"run"         description:"原生启动命令"`        // 原生启动命令
+	Script      string `json:"script"      orm:"script"         description:"sh脚本启动命令"`   // sh脚本启动命令
 	Status      int    `json:"status"      orm:"status"      description:"状态[1:启动，0:停止]"` // 状态[1:启动，0:停止]
 	Description string `json:"description" orm:"description" description:"项目描述"`          // 项目描述
 }
@@ -22,7 +23,7 @@ type Jpid struct {
 type LinuxPid struct {
 	Name    string `json:"name"        orm:"name"        description:"java项目名"`         // java项目名
 	Pid     int    `json:"pid"         orm:"pid"         description:"pid"`             // pid
-	Run     string `json:"run"         orm:"run"         description:"运行脚本（sh命令"`       // 运行脚本（sh命令
+	Run     string `json:"run"         orm:"run"         description:"原生启动命令"`          // 原生启动命令
 	Ports   string `json:"ports"    orm:"ports"       description:"占用的端口"`              // 占用的端口
 	Catalog string `json:"catalog"     orm:"catalog"     description:"运行目录[jar文件所在目录]"` // 运行目录[jar文件所在目录]
 }
