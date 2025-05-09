3b6537b50927c97bff09d00a9a92285d6a3b5ffe
tan <1445763190@qq.com>
2025-05-09 12:58:55 +0800

    修改接口前缀

--- api/jpid/v1/jpid.go
+++ api/jpid/v1/jpid.go
@@ -13,21 +13,21 @@ type JpidRes struct {
 }
 
 type OnlineReq struct {
-	g.Meta `path:"/findOnline" tags:"Java" method:"get" summary:"查询在线的java项目列表"`
+	g.Meta `path:"/jpid/findOnline" tags:"Java" method:"get" summary:"查询在线的java项目列表"`
 }
 type OnlineRes struct {
 	List []*entity.LinuxPid `json:"list" dc:"在线java项目"`
 }
 
 type AutoRegisterReq struct {
-	g.Meta `path:"/auto/register" tags:"Java" method:"get" summary:"自动注册在线的java项目列表"`
+	g.Meta `path:"/jpid/auto/register" tags:"Java" method:"get" summary:"自动注册在线的java项目列表"`
 }
 type AutoRegisterRes struct {
 	Message string `json:"message" dc:"注册成功"`
 }
 
 type StopProjectReq struct {
-	g.Meta `path:"/stop/:pid" tags:"Java" method:"post" summary:"根据pid停止运行"`
+	g.Meta `path:"/jpid/stop/:pid" tags:"Java" method:"post" summary:"根据pid停止运行"`
 	Pid    int `v:"required|min:1" json:"pid" dc:"进程ID"`
 }
 type StopProjectRes struct {
@@ -35,7 +35,7 @@ type StopProjectRes struct {
 }
 
 type StartWithRunReq struct {
-	g.Meta `path:"/start/run/:pid" tags:"Java" method:"post" summary:"原生命令启动"`
+	g.Meta `path:"/jpid/start/run/:pid" tags:"Java" method:"post" summary:"原生命令启动"`
 	Pid    int `v:"required|min:1" json:"pid" dc:"进程ID"`
 }
 type StartWithRunRes struct {
@@ -44,7 +44,7 @@ type StartWithRunRes struct {
 }
 
 type StartWithScriptReq struct {
-	g.Meta `path:"/start/script/:pid" tags:"Java" method:"post" summary:"脚本启动"`
+	g.Meta `path:"/jpid/start/script/:pid" tags:"Java" method:"post" summary:"脚本启动"`
 	Pid    int `v:"required|min:1" json:"pid" dc:"进程ID"`
 }
 
