2129c3e49120509d3fb31626811c4f6af75e6668
tan <13320385140@163.com>
2025-05-19 16:33:10 +0800

    新增结构

--- doc/table.sql
+++ doc/table.sql
@@ -6,8 +6,9 @@ CREATE TABLE `jpid` (
                         `catalog` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '运行目录',
                         `run` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT '原生启动命令',
                         `script` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT 'sh脚本启动命令',
-                        `worker` varchar(50) COLLATE utf8mb4_general_ci NOT NULL COMMENT '服务器',
+                        `worker` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '服务器',
                         `status` int DEFAULT '0' COMMENT '状态[1:启动，0:停止]',
                         `description` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '项目描述',
+                        `docker` int DEFAULT '2' COMMENT 'docker[1:docker, 2jdk]',
                         PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='java项目详情';
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='java项目详情';
